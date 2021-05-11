99ed08ed64294aee60d96f9849f37cbf88ab6b11
GTL <GTL@DESKTOP-0LQB8T5>
2021-05-11 13:10:42 -0300

    task: se agregó el arrays para Compra

---
+++ Prog_Visual_TP5_2021/src/main/java/ar/edu/unju/fi/tp5/util/TablaCompra.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.tp5.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TablaCompra {
+	public static List<Compra> listaCompras = new ArrayList<>();
+
+}
